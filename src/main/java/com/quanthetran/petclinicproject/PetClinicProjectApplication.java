3dfeab302734c8365c1da6e66e85db99b2841484
QUAN THE TRAN <qtran88@cs.washington.edu>
2018-10-27 22:04:22 -0700

    intial Project Add

---
+++ src/main/java/com/quanthetran/petclinicproject/PetClinicProjectApplication.java
@@ -0,0 +1,12 @@
+package com.quanthetran.petclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicProjectApplication.class, args);
+    }
+}
